6447a86d4faa603232404833e962c8bd53ae07b6
sabuz796 <sabuz796@gmail.com>
2023-07-05 18:02:33 +0200

    added changing hamburger menu btn while clicking

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ const hamburgerMenu = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav-list");
 
 hamburgerMenu.addEventListener("click", () => {
+  //changing hamburger menu while clicking
+  hamburgerMenu.classList.toggle("ph-x");
   navList.classList.toggle("menu-show");
 });
